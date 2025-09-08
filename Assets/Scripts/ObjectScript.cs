e766672bf43a4372d4e2bf701be7681babc4c10b
maks1k3 <maksbizjuk@gmail.com>
2025-09-08 14:37:50 +0300

    End of the day 1

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+    public GameObject[] vehicles;
+    [SerializeField]
+    public Vector2[] startCoordinates;
+    public Canvas can;
+    public AudioSource effects;
+    public AudioClip[] audioCli;
+    [HideInInspector]
+    public bool rightPlace = false;
+    public GameObject lastDragged = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
